1056765793050abd49b2ade10c506b3cd84a0558
Sofia Mesa <sofiamesa@my.whccd.edu>
2025-01-15 19:58:57 -0800

    Add project files.

---
+++ COMP003.LectureActivity1/Program.cs
@@ -0,0 +1,13 @@
+﻿//namespace level
+namespace COMP003.LectureActivity1
+{
+    // class level
+    internal class Program
+    {
+        // mail level
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
